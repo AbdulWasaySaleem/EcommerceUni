198ea7878f430e41753718ee42b91d1ccc8d78f5
AbdulWasaySaleem <153143400+AbdulWasaySaleem@users.noreply.github.com>
2025-04-29 14:15:50 +0500

    added welcome

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.use(express.json());
 app.use("/api/user", userRoutes); // example: /api/user/register, /api/user/login
 app.use("/api/products", productRoutes);
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-commerce API");
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
